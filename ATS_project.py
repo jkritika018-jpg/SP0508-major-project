801f45e44d49bcb2a9628f030314edb6171c1a1a
Kritika <jkritika018@gmail.com>
2026-02-16 01:36:50 +0530

    Added a feature to upload a resume

--- ATS_project.py
+++ ATS_project.py
@@ -9,7 +9,7 @@ from PIL import Image
 import pdf2image
 import google.generativeai as genai
 
-genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
+genai.configure(api_key=os.getenv("AIzaSyDVYmG2E5ev084il15nXwd-coP0_1BgPSw"))
 
 def get_gemini_response(input,pdf_cotent,prompt):
     model=genai.GenerativeModel('gemini-pro-vision')
